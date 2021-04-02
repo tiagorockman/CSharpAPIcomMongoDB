2e2171259079481dd479306599a0124b8eb8d27b
Jose Sousa (Contractor) <tiagorockman@gmail.com>
2021-04-02 13:50:33 -0300

    Iniciando repositorio

---
+++ APIcomMongoDB/Controllers/InfectadoController.cs
@@ -0,0 +1,63 @@
+﻿using APIcomMongoDB.Data.Collection;
+using APIcomMongoDB.Model;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIcomMongoDB.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+            Data.MongoDB _mongoDB;
+            IMongoCollection<Infectado> _infectadosCollection;
+
+            public InfectadoController(Data.MongoDB mongoDB)
+            {
+                _mongoDB = mongoDB;
+                _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+            }
+
+            [HttpPost]
+            public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+            {
+                var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+                _infectadosCollection.InsertOne(infectado);
+
+                return StatusCode(201, "Infectado adicionado com sucesso");
+            }
+
+            [HttpGet]
+            public ActionResult ObterInfectados()
+            {
+                var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+
+                return Ok(infectados);
+            }
+
+            [HttpPut]
+            public ActionResult AtualizarInfectado([FromBody] InfectadoDto dto)
+            {
+                // atualizaçao por data de nascimento, o ideal seria que fosse por id
+                //atualizando apenas uma propriedade
+                _infectadosCollection.UpdateOne(Builders<Infectado>.Filter.Where(_ => _.DataNascimento == dto.DataNascimento), Builders<Infectado>.Update.Set("sexo", dto.Sexo));
+
+                return Ok("Atualizado com sucesso");
+            }
+
+
+            [HttpDelete("{}data")]
+            public ActionResult Delete(DateTime dataNascimento)
+            {
+                // atualizaçao por data de nascimento, o ideal seria que fosse por id
+                _infectadosCollection.DeleteOne(Builders<Infectado>.Filter.Where(_ => _.DataNascimento == dataNascimento));
+
+                return Ok("Deletado com sucesso");
+            }
+    }
+}
---
+++ APIcomMongoDB/Startup.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIcomMongoDB
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(s =>
+           {
+               s.SwaggerDoc("v1", new OpenApiInfo { Title = "ApicomMongoDB", Version = "v1" });
+           });
+            //Adicionando a injecao de dependencia para a InfectadoController.cs receber os dados do MONGO
+            services.AddSingleton<Data.MongoDB>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApicomMongoDB"));
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
